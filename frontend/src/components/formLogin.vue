a9d72daeac917c26667cdbde71217dc2af24126f
EugenioAloi <60814811+eugenioaloi@users.noreply.github.com>
2023-09-16 00:40:28 +0200

    added vuetify and first login-form with simple logic

---
+++ frontend/src/components/formLogin.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="d-flex align-center justify-center" style="height: 100vh">
+      <v-sheet width="400" class="mx-auto">
+          <v-form @submit.prevent="submitForm">
+              <v-text-field 
+              v-model="username" label="User Name"
+              />
+              <v-text-field 
+              v-model="password" label="password"/>
+              <v-btn 
+              type="submit" 
+              color="primary" 
+              block class="mt-2">
+                  Login to portal
+              </v-btn>
+          </v-form>
+          <div class="mt-2">
+              <p class="text-body-2">Don't have an account? 
+                  <a href="#">Sign Up</a>
+              </p>
+          </div>
+      </v-sheet>
+  </div>
+</template>
+
+<script>
+
+export default{
+
+  data() {
+      return {
+          username: '',
+          password: '',
+      };
+  },
+  methods: {
+      submitForm(){
+          console.log("username", this.username);
+          console.log("password", this.password);
+      }, 
+  },
+}
+
+</script>
